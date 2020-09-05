09425e6561486e2de94e8faaa621bace51427d46
Leidy Rojas <leidyrojas@MacBook-Pro-de-Luis.local>
2020-09-05 13:24:37 -0500

    Fix cambio resolución Trending Gifos JS

--- js/trendings_slider.js
+++ js/trendings_slider.js
@@ -1,16 +1,16 @@
 //Este archivo tiene como finalidad mostrar los gif que son tendencias (slider) en Giphy
 const trendingsGifs = 'https://api.giphy.com/v1/gifs/trending';
-const limitMobile = 'limit=12';
-const limitDesktop = 'limit=3';
+const limitMobile = 12;
+const limitDesktop = 3;
 const offset = 0;
 const rating = 'rating=g';
 
-const trendings_url = ( trendingsGifs + '?' + KEY + '&' + rating );
+const trendings_url = (trendingsGifs + '?' + KEY + '&' + rating);
 
 
 
-function getTrendings() {
-    let url = trendingsGifs+ "?" + KEY + "&limit=12&rating=g"; // esta es la dirección para obtener de a 3 trendings
+function getTrendings(limit) {
+    let url = trendingsGifs + "?" + KEY + "&limit=" + limit + "&rating=g"; // esta es la dirección para obtener de a 3 trendings
     // Request es un objeto que describe que tipo de operación requiero y qué condiciones debe cumplir para que sea exitoso
     // Para efectos de esta función debo solicitar información por los cual es un GET y espero un JSON como respuesta
     let request = {
@@ -28,12 +28,7 @@ function getTrendings() {
             }
         })
         .then(json => { //Paso 2: dado que fue un http-200 obtengo el json de Giphy con los trends
-            if (screen.width > 768) {
-                loadTrends(json, 3);
-            }
-            else {
-                loadTrends(json, 12);
-            }
+            loadTrends(json, limit);
         })
         .catch(function (error) { // En caso que alguno de los pasos falle capturo el error para analizar qué ocurrio
             //console.error("Error en la petición" + error.message);
@@ -50,5 +45,20 @@ function loadTrends(json, limit) {
     }
 }
 
+
+//Event para que los trending Gifos se recarguen de acuerdo a la resolución de la pantalla en el Slider
+function displayTrendingGifs() {
+    const w = document.documentElement.clientWidth;
+    if (w >= 768) {
+        getTrendings(limitDesktop);
+    }
+    else {
+        getTrendings(limitMobile);
+    }
+}
+
+// Anexar al evento el listener de la función del cambio de resolución
+window.addEventListener("resize", displayTrendingGifs);
+
 //Muestran los trendings
-getTrendings();
+displayTrendingGifs();
