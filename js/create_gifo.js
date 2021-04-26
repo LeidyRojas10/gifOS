58f259b73f7b34836a4691eb32e00e20fc11bfd5
Leidy Rojas <leidyrojas@MacBook-Pro-de-Luis.local>
2021-04-25 19:20:43 -0500

    crear gifos en proceso v2

--- js/create_gifo.js
+++ js/create_gifo.js
@@ -1,21 +1,36 @@
 
 //Key para el sesionstorage
 const gifosCreationStepKey = 'gifosCreationStep';
+const video = document.getElementById('gifos_video');
 
-
-//Botón para crear Gifo
+//Botón para comenzar 'crear Gifo'
 const startCreationButton = document.getElementById('start_creation');
+
+//Botón para iniciar la grabación para 'crear Gifo'
+const recordButton = document.getElementById('record');
+
+//Botón para finalizar la grabación para 'crear Gifo'
+const stopRecordingButton = document.getElementById('stop_recording');
+
+//Botón para subir a Gifos el Gif que hemos creado
+const uploadButton = document.getElementById('upload');
+
+
 
 //Función para solicitar permisos de acceso a la cámara 
 const getVideo = () => {
-    const videoPromise = navigator.mediaDevices.getUserMedia({ video: true });
-    videoPromise.then(function (mediaStream) {
-        let video = document.getElementById('gifos_video');
-        video.srcObject = mediaStream;
-        video.play();
 
+    const videoPromise = navigator.mediaDevices.getUserMedia({ video: true });
+    videoPromise.then(async function (mediaStream) {
+        showVideo(mediaStream);
     });
-    videoPromise.catch(function (err) { console.log(err.name); }); // always check for errors at the end.
+    videoPromise.catch(function (err) { console.log(err.name); }); 
+}
+
+//Función para mostrar en video lo obtenido desde la cámara
+const showVideo = (mediaStream) =>{
+    video.srcObject = mediaStream;
+    video.play();
 }
 
 //Agregar evento clicl al botón Comenzar creación del Gifo
