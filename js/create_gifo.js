aa09a5663aeea37ba8fb08dc6fe33537db7e98f7
Leidy Rojas <leidyrojas@MacBook-Pro-de-Luis.local>
2021-04-25 21:09:32 -0500

    se crea recorder js

--- js/create_gifo.js
+++ js/create_gifo.js
@@ -15,26 +15,43 @@ const stopRecordingButton = document.getElementById('stop_recording');
 //Botón para subir a Gifos el Gif que hemos creado
 const uploadButton = document.getElementById('upload');
 
-
-
 //Función para solicitar permisos de acceso a la cámara 
 const getVideo = () => {
-
     const videoPromise = navigator.mediaDevices.getUserMedia({ video: true });
     videoPromise.then(async function (mediaStream) {
         showVideo(mediaStream);
+        const recorder = createRecorder(mediaStream);
     });
-    videoPromise.catch(function (err) { console.log(err.name); }); 
+    videoPromise.catch(function (err) { console.log(err.name); });
 }
 
 //Función para mostrar en video lo obtenido desde la cámara
-const showVideo = (mediaStream) =>{
+const showVideo = (mediaStream) => {
     video.srcObject = mediaStream;
     video.play();
 }
+
+//Función para crear el recorder
+const createRecorder = (mediaStream) => {
+    const recorder = RecordRTC(mediaStream, {
+        type: 'gif',
+        frameRate: 1,
+        quality: 10,
+        width: 360,
+        hidden: 240,
+        onGifRecordingStarted: function () {
+            console.log('started')
+        },
+    });
+
+    return recorder;
+}
+
+
 
 //Agregar evento clicl al botón Comenzar creación del Gifo
 startCreationButton.addEventListener('click', (event) => {
     event.preventDefault();
     getVideo();
 })
+
